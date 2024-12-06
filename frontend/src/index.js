ed105c07b0abaf53afe9a8bf11a5fbc421199d1a
snehakumari8801 <snehakumari8801@gmail.com>
2024-12-06 16:00:11 +0530

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router";
 import ChatProvider from "./context/ChatProvider";
-// import { ChakraProvider } from "@chakra-ui/react";
 
-// Render the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ChatProvider>
-      {/* <ChakraProvider> */}
       <App />
-      {/* </ChakraProvider> */}
     </ChatProvider>
   </BrowserRouter>
 );
