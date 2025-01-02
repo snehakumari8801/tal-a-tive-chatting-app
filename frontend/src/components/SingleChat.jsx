628ca8109edaf81a58c345d1dd0bfb012058315f
snehakumari8801 <snehakumari8801@gmail.com>
2025-01-02 20:06:14 +0530

    update

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -497,7 +497,7 @@ import animationData from "../../src/typing.json";
 import io from "socket.io-client";
 import { FaArrowLeft } from "react-icons/fa";
 
-const ENDPOINT = 'https://tal-a-tive-chatting-app-21.onrender.com'           //"http://localhost:3000"; // Your backend endpoint
+const ENDPOINT = "http://localhost:3000"  //'https://tal-a-tive-chatting-app-21.onrender.com'        //; // Your backend endpoint
 let socket;
 
 function SingleChat({
