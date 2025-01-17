771acb631c7a3e9311426aa62eefc2ee6310f5ae
snehakumari8801 <snehakumari8801@gmail.com>
2025-01-17 20:29:23 +0530

    change origin

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const server = app.listen(port, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin:"http://localhost:3001",
+    origin:"*",
   },
 });
 
